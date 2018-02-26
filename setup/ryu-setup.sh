5ba96c16099cfc16b1a8d4ffa4c88873b3e2dca4
Giacomo Verticale <giacomo.verticale@polimi.it>
2018-02-26 22:16:07 +0100

    fix typo in apt command

--- setup/ryu-setup.sh
+++ setup/ryu-setup.sh
@@ -1,6 +1,6 @@
-sudo apt get gcc install python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip
+sudo apt install -y gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip
+
 git clone git://github.com/osrg/ryu.git 
 pushd ryu
-# sudo python ./setup.py install 
 pip install .
 popd
