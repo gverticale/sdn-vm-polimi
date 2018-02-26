1d760964ee163a6ca07e27b413e47121a3cb16fc
Giacomo Verticale <giacomo.verticale@polimi.it>
2018-02-26 22:16:40 +0100

    Use ovs from the repository

--- setup/mininet-setup.sh
+++ setup/mininet-setup.sh
@@ -1,6 +1,8 @@
+sudo apt install -y openvswitch-switch
+
 git clone git://github.com/mininet/mininet
 pushd mininet
 git checkout
 popd
-mininet/util/install.sh -nfv
-# mininet/util/install.sh -y
+mininet/util/install.sh -n
+# mininet/util/install.sh -nfv
