b823f77696d050b6175f362273bda1edc0f3a1db
Giacomo Verticale <giacomo.verticale@polimi.it>
2018-05-08 18:26:02 +0200

    Monitoring

--- sdn-lab/5 - monitoring/switch-monitor.py
+++ sdn-lab/5 - monitoring/switch-monitor.py
@@ -4,18 +4,45 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet
+from ryu.lib import hub
 
 # This implements a learning switch in the controller
-# The switch sends all packets to the controller
+# The switch sends all packet to the controller
 # The controller implements the MAC table using a python dictionary
-# If the MAC dst is known, add rule to the switch
+# The controller prints flow statistics
+
+# eseguire con:
+# ryu-manager --verbose
+
 class PsrSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(PsrSwitch, self).__init__(*args, **kwargs)
+
+        # tabella dei MAC
         self.mac_to_port = {}
-	
+
+        # tabella dei datapath
+        self.datapaths = {}
+
+        # thread che lancia periodicamente le richieste
+        self.monitor_thread = hub.spawn(self._monitor)
+
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stats(dp)
+            hub.sleep(10)
+
+    def _request_stats(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        datapath.send_msg(req)
+            
     # execute at switch registration
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -23,9 +50,12 @@ class PsrSwitch(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        self.datapaths[datapath.id] = datapath
         self.mac_to_port.setdefault(datapath.id, {})
 		
+        # match all packets 
         match = parser.OFPMatch()
+        # send to controller
         actions = [
             parser.OFPActionOutput(
                 ofproto.OFPP_CONTROLLER,
@@ -36,8 +66,8 @@ class PsrSwitch(app_manager.RyuApp):
             parser.OFPInstructionActions(
                 ofproto.OFPIT_APPLY_ACTIONS,
                 actions
-            )
-        ]
+           )
+       ]
         mod = parser.OFPFlowMod(
             datapath=datapath,
             priority=0,
@@ -109,4 +139,34 @@ class PsrSwitch(app_manager.RyuApp):
             )
 
         datapath.send_msg(ofmsg)
-        
+            
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        self.logger.info('datapath         '
+        'match        '
+        'out-port packets bytes')
+        self.logger.info('---------------- '
+        '-------- ----------------- '
+        '-------- -------- --------')        
+        for stat in body:
+            self.logger.info('%016x %26s %8x %8d %8d',
+                ev.msg.datapath.id,
+                stat.match,
+                stat.instructions[0].actions[0].port,
+                stat.packet_count, stat.byte_count)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        self.logger.info('datapath port '
+        'rx-pkts rx-bytes rx-error '
+        'tx-pkts tx-bytes tx-error')
+        self.logger.info('---------------- -------- '
+        '-------- -------- -------- '
+        '-------- -------- --------')
+        for stat in body:
+            self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+                ev.msg.datapath.id, stat.port_no,
+                stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+                stat.tx_packets, stat.tx_bytes, stat.tx_errors)
---
+++ sdn-lab/6 - mpls/mpls.py
@@ -0,0 +1,114 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+# Topologia di riferimento
+# sudo mn –mac –arp –topo linear,3 –switck ovsk,datapath=user –controller remote
+#       s1 -- s2 -- s3
+#     /        |      \
+#    h1       h2       h3
+# I pacchetti da h1 a h3 sono etichettati in s1 con etichetta 1000
+# I pacchetti da h3 a h1 sono etichettati in s3 con etichetta 1001
+# In s1 inserire regola:
+# ** destinazione 10.0.0.3/32 => push 1000, next-hop=s2
+# In s2 insirire la regola:
+# ** 1000 => 1000, next-hop=s3
+# In s3 inserire regola:
+# ** destinazine 10.0.0.1/32 => push 1001, next-hop=s2
+
+
+class Mpls(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    # execute at switch registration
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # match all packets
+        match = parser.OFPMatch()
+        # send to controller
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=0,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+        if datapath.id == 1:
+
+            match = parser.OFPMatch(eth_type=0x0800, ipv4_dst="10.0.0.3")
+            actions = [
+                parser.OFPActionPushMpls(),
+                parser.OFPActionSetField(mpls_label=1000),
+                parser.OFPActionOutput(2)
+            ]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+            mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                match=match, instructions=inst)
+            datapath.send_msg(mod)
+
+            match = parser.OFPMatch(eth_type=0x8847, mpls_label=1001)
+            actions = [
+                parser.OFPActionPopMpls(),
+                parser.OFPActionOutput(1)
+            ]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+            mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                match=match, instructions=inst)
+            datapath.send_msg(mod)
+
+
+        if datapath.id == 2:
+
+            match = parser.OFPMatch(eth_type=0x8847, mpls_label=1000)
+            actions = [
+                parser.OFPActionOutput(3)
+            ]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+            mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                match=match, instructions=inst)
+            datapath.send_msg(mod)
+
+            match = parser.OFPMatch(eth_type=0x8847, mpls_label=1001)
+            actions = [
+                parser.OFPActionOutput(2)
+            ]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+            mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                match=match, instructions=inst)
+            datapath.send_msg(mod)
+
+        if datapath.id == 3:
+
+            match = parser.OFPMatch(eth_type=0x8847, mpls_label=1000)
+            actions = [
+                parser.OFPActionPopMpls(),
+                parser.OFPActionOutput(1)
+            ]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+            mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                match=match, instructions=inst)
+            datapath.send_msg(mod)
+
+            match = parser.OFPMatch(eth_type=0x0800, ipv4_dst="10.0.0.1")
+            actions = [
+                parser.OFPActionPushMpls(),
+                parser.OFPActionSetField(mpls_label=1001),
+                parser.OFPActionOutput(2)
+            ]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+            mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                match=match, instructions=inst)
+            datapath.send_msg(mod)
