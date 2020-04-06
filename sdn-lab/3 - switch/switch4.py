0795668fdc6e85f7643d34cf6d93e6076619cba1
Giacomo Verticale <giacomo.verticale@polimi.it>
2020-04-06 20:15:33 +0200

    Switch hop-by-hop

---
+++ sdn-lab/3 - switch/switch4.py
@@ -0,0 +1,122 @@
+# Implementazione openflow di hop-by-hop routing
+# usando la mappa della rete trovata con topology discovery
+#
+# Si richiede l'uso del topology discovery
+# ryu-manager --observe-links
+#
+# Nella versione attuale richiede risoluzione arp gia' fatta
+# invocare mininet con
+# mn --arp
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls, CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.ofproto import ofproto_v1_3
+from ryu.topology import event, switches
+from ryu.topology.api import get_all_switch, get_all_link, get_all_host
+from ryu.lib.packet import packet, ethernet, ether_types
+import networkx as nx
+
+
+class HopByHopSwitch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    # tutti i pacchetti al controllore
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                [
+                    parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,128)
+                ]
+            )
+        ]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=0,
+            match = parser.OFPMatch(),
+            instructions=inst
+        )
+        datapath.send_msg(mod)
+
+    # trova switch destinazione e porta dello switch
+    def find_destination_switch(self,destination_mac):
+        for host in get_all_host(self):
+            if host.mac == destination_mac:
+                return (host.port.dpid, host.port.port_no)
+        return (None,None)
+
+    def find_next_hop_to_destination(self,source_id,destination_id):
+        net = nx.DiGraph()
+        for link in get_all_link(self):
+            net.add_edge(link.src.dpid, link.dst.dpid, port=link.src.port_no)
+
+        path = nx.shortest_path(
+            net,
+            source_id,
+            destination_id
+        )
+
+        first_link = net[ path[0] ][ path[1] ]
+
+        return first_link['port']
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        # ignora pacchetti non IPv4 (es. ARP, LLDP)
+        if eth.ethertype != ether_types.ETH_TYPE_IP:
+            return
+
+        destination_mac = eth.dst
+
+        # trova switch destinazione
+        (dst_dpid, dst_port) = self.find_destination_switch(destination_mac)
+
+        # host non trovato
+        if dst_dpid is None:
+            # print "DP: ", datapath.id, "Host not found: ", pkt_ip.dst
+            return
+
+        if dst_dpid == datapath.id:
+            # da usare se l'host e' direttamente collegato
+            output_port = dst_port    
+        else:
+            # host non direttamente collegato
+            output_port = self.find_next_hop_to_destination(datapath.id,dst_dpid)
+
+        # print "DP: ", datapath.id, "Host: ", pkt_ip.dst, "Port: ", output_port
+
+        # aggiungi la regola
+        match = parser.OFPMatch(
+            eth_dst=destination_mac
+            )
+        inst = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                [ parser.OFPActionOutput(output_port) ]
+            )
+        ]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=10,
+            match=match,
+            instructions=inst,
+            buffer_id=msg.buffer_id
+        )
+        datapath.send_msg(mod)
+
+        return
